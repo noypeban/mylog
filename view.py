23adfeceefb6af7c384afa799d6e273dee537cdf
noypeban <noyp.eban@gmail.com>
2014-07-29 18:23:22 +0900

    Update view.py

--- view.py
+++ view.py
@@ -12,7 +12,6 @@ import sqlite3
 import locale
 locale.setlocale(locale.LC_ALL, '')
 
-
 class mylog(object):
     def __init__(self):
         self.dirpath = os.path.dirname(os.path.abspath(__file__))
@@ -23,32 +22,45 @@ class mylog(object):
 
     def get_entry(self,id=1):
         entry = self.connection.execute(u"select * from entry where id=%s" % id).fetchone()
-        category = map(lambda lst: lst[0], self.connection.execute(
-                    u"select category from category where id=%s" % entry[0])
-                    .fetchall())
+        if entry is not None:
+            category = map(lambda lst: lst[0], self.connection.execute(
+                        u"select category from category where id=%s" % entry[0])
+                        .fetchall())
+        else:
+            category = None
         return entry, category
 
     def curses_main(self,args):
         win = curses.initscr()
         curses.noecho()
         curses.cbreak()
-        entry, category = self.get_entry(1)
-
-        entry_date = entry[1].strftime('%Y/%m/%d')
-        category = "category:" + ",".join(category)
-
-        win.addstr(0,0,entry_date)
-        win.addstr(1,0,category)
-        win.addstr(3,0,entry[2])
+        lastid = self.connection.execute(u"select max(id) from entry").fetchone()[0]
+        id = self.display_entry(win, lastid)
         while 1:
             c = win.getkey()
-            if c == "c":
-                win.erase()
+            if c == "n":
+                id = self.display_entry(win, id, -1)
+            elif c == "p":
+                id = self.display_entry(win, id, 1)
             elif c == "q":
                 break
             win.refresh()
         curses.endwin()
         self.connection.close()
+    def display_entry(self,win,id,offset=0):
+        entry, category = self.get_entry(id+offset)
+
+        if entry is not None:
+            win.erase()
+            entry_date = entry[1].strftime('%Y/%m/%d%A %H:%M:%S')
+            category = "category:" + ",".join(category)
+
+            win.addstr(0,0,entry_date)
+            win.addstr(1,0,category)
+            win.addstr(3,0,entry[2])
+            return entry[0]
+        else:
+            return id
 
 if __name__ == '__main__':
     a = mylog()
